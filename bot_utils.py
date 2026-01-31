33c58f4fa4ed077fc9d56442e7f24a787bd209d2
minemixcraft <mixmu531@gmail.com>
2026-01-31 09:33:26 +0700

    Update bot_utils.py

--- bot_utils.py
+++ bot_utils.py
@@ -148,7 +148,9 @@ def run_autopost_workflow(bot_name, bot_data, hashtag_pool):
         wait_for_schedule_start(context['target_hour'])
 
         # Random Delay
-        apply_random_delay(context['max_wait_min'])
+        # apply_random_delay(context['max_wait_min'])
+        apply_random_delay(1)
+        
 
         # Prepare Content
         client, api_v1 = get_twitter_client()
@@ -176,4 +178,5 @@ def run_autopost_workflow(bot_name, bot_data, hashtag_pool):
     
     # --- END SECTION ---
     bot_ui.print_end()
+
 
