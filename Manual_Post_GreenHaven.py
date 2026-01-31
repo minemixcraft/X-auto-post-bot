37f863ff19d20ff8575b12daa01b2387a1090208
minemixcraft <mixmu531@gmail.com>
2026-01-31 19:08:54 +0700

    Add files via upload

---
+++ Manual_Post_GreenHaven.py
@@ -0,0 +1,8 @@
+# Import ข้อมูลตั้งค่า
+from bot_config import SHARED_HASHTAGS, GREENHAVEN_DATA
+# Import ระบบโพสต์กลาง
+from bot_utils import run_manual_workflow
+
+if __name__ == "__main__":
+    # สั่งให้ระบบทำงาน โดยส่งชื่อและข้อมูลของ GreenHaven เข้าไป
+    run_manual_workflow("GreenHaven", GREENHAVEN_DATA, SHARED_HASHTAGS)
