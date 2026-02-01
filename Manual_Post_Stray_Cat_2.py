94104bf03c55b8c60fac365ebc754f6e7f21e04c
minemixcraft <mixmu531@gmail.com>
2026-02-01 18:10:15 +0700

    Update Manual_Post_Stray_Cat_2.py

--- Manual_Post_Stray_Cat_2.py
+++ Manual_Post_Stray_Cat_2.py
@@ -1,8 +1,9 @@
 # Import ข้อมูลตั้งค่า
-from bot_config import SHARED_HASHTAGS, GREENHAVEN_DATA
+from bot_config import SHARED_HASHTAGS, STRAY_CAT_DATA
 # Import ระบบโพสต์กลาง
 from bot_utils import run_manual_workflow
 
 if __name__ == "__main__":
     # สั่งให้ระบบทำงาน โดยส่งชื่อและข้อมูลของ GreenHaven เข้าไป
     run_manual_workflow("Stray Cat 2", STRAY_CAT_DATA, SHARED_HASHTAGS)
+
